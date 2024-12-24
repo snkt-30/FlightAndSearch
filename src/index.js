83896681f1fde21b6c9ca65310da3bc8cbf5215f
SANKET KHONDE <khondesanket.30@ibm.com>
2024-12-24 15:42:33 +0530

    basic bodyparser setup added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const {PORT} = require('./config/serverConfig');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const setupAndStartServer = async()=>{
+const { PORT } = require("./config/serverConfig");
 
-    const app = express();
-    app.listen(PORT,()=>{
-        // console.log(process.env); to see our env variable
-        console.log(`Server Started at ${PORT}`)
-    })
-}
+const setupAndStartServer = async () => {
+
+    // create a express object
+  const app = express();
+
+  //body parser setup
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    // console.log(process.env); to see our env variable
+    console.log(`Server Started at ${PORT}`);
+  });
+};
 
 setupAndStartServer();
