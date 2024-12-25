e31544d420892e941fa3d05c624935fcf30f7efc
SANKET KHONDE <khondesanket.30@ibm.com>
2024-12-25 16:26:20 +0530

    city-service logic added

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,23 +1,57 @@
 const { City } = require("../models/index");
 
 class CityRepository {
+
+  // register a new city 
   async createCity({ name }) {
     try {
-        const city = await City.createCity({name});
-        return city;
+      // const city = await City.createCity({name});
+      const city = await City.createCity({
+        name: name,
+      });
+      return city;
     } catch (error) {
-        throw (error);
+      console.log("something went wrong in repository level");
+      throw { error };
     }
   }
-  async deleteCity(cityId){
+
+  // Get the city by cityId
+  async getCity(cityId) {
     try {
-        await City.destroy({
-            where:{
-                id:cityId
-            }
-        });
+      const city = await City.findByPk(cityId);
+      return city;
     } catch (error) {
-        throw (error);
+      console.log("something went wrong in repository level");
+      throw { error };
+    }
+  }
+
+  //update the city values
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      console.log("something went wrong in repository level");
+      throw { error };
+    }
+  }
+
+  // delete the city 
+  async deleteCity(cityId) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      console.log("something went wrong in repository level");
+      throw { error };
     }
   }
 }
---
+++ src/services/city-service.js
@@ -0,0 +1,47 @@
+const { CityRepository } = require("../repository/index");
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in service level");
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const city = await this.cityRepository.deleteCity(cityId);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in service level");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await this.cityRepository.updateCity(cityId, data);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in service level");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(cityId);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in service level");
+      throw { error };
+    }
+  }
+}
