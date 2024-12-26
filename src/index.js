00fe991e3d902d25dc101c225fe51308bba31994
SANKET KHONDE <khondesanket.30@ibm.com>
2024-12-26 17:37:33 +0530

    DB_SYNC logic in index.js added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./Routes/index");
 
+const db = require("./models/index");
+const sequelize = require("sequelize");
 
 const setupAndStartServer = async () => {
   // create a express object
@@ -15,9 +17,35 @@ const setupAndStartServer = async () => {
 
   app.use("/api", apiRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     // console.log(process.env); to see our env variable
     console.log(`Server Started at ${PORT}`);
+
+    if(process.env.SYNC_DB){
+      db.sequelize.sync({ alter: true });
+    }
+
+    // const airport = await db.Airport.create({
+    //   name: "Kempegouda Internation Airport",
+    //   cityId: 3,
+    // });
+
+    // const airport = await db.Airport.findOne({
+    //   where: {
+    //     cityId: 3,
+    //   },
+    // });
+
+
+    // const city = await db.City.findOne({
+    //   where: {
+    //     id: 10,
+    //   },
+    // });
+    // const res = await city.getAirports();
+    // console.log(res);
+
+
   });
 };
 
